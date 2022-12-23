f0bb8951a442017f0e2e3bd480451a061c2f05bb
AdamEli <50723103+AdamEli@users.noreply.github.com>
2022-12-23 00:48:39 -0500

    Added responsive mobile nav

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
-const hamburger = document.querySelector(".mobile-nav-toggle");
-const nav = document.querySelector(".nav");
+const hamburger = document.querySelector("#hamburger");
+const nav = document.querySelector("#nav");
+const li = document.querySelectorAll(".nav-element");
+const body = document.querySelector("body");
+console.log(li);
 hamburger.addEventListener("click", () => {
   nav.toggleAttribute("data-visible");
+  body.toggleAttribute("data-visible");
 });
+
+li.forEach(function (elem) {
+  elem.addEventListener("click", function () {
+    nav.toggleAttribute("data-visible");
+    body.toggleAttribute("data-visible");
+  });
+});
+// document.onclick = function (clickEvent) {
+//   console.log(nav.hasAttribute("data-visible"));
+//   if (nav.hasAttribute("data-visible") && clickEvent.target.id !== "nav") {
+//     nav.toggleAttribute("data-visible");
+//   }
+// };
